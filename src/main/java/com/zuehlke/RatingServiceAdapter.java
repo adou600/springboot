a17234ad93b589a3dca16fc0ef57bdf61d3a8f40
Adrien Nicolet <adrien.nicolet@zuehlke.com>
2017-05-10 16:38:28 +0200

    design for failure

--- src/main/java/com/zuehlke/RatingServiceAdapter.java
+++ src/main/java/com/zuehlke/RatingServiceAdapter.java
@@ -3,10 +3,9 @@ package com.zuehlke;
 
 import com.zuehlke.data.Rating;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 
+import java.util.Collections;
 import java.util.List;
 
 @Component
@@ -15,7 +14,9 @@ public class RatingServiceAdapter {
     private final RatingServiceApiClient ratingsApiClient;
 
     public RatingServiceAdapter(@Value("${endpoint.movie-rating-service}") String url) {
-        ratingsApiClient = RestClientFactory.createClient(url, RatingServiceApiClient.class);
+
+        RatingServiceApiClient fallback = (id) -> Collections.emptyList();
+        ratingsApiClient = RestClientFactory.createClient(url, RatingServiceApiClient.class, fallback);
     }
 
     public List<Rating> getRatingsById(long id) {
--- src/main/java/com/zuehlke/RestClientFactory.java
+++ src/main/java/com/zuehlke/RestClientFactory.java
@@ -4,13 +4,23 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Logger;
+import feign.hystrix.FallbackFactory;
 import feign.hystrix.HystrixFeign;
 import feign.jackson.JacksonDecoder;
 import feign.slf4j.Slf4jLogger;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+
 // Feign RestClientFactory
 public class RestClientFactory {
+
     public static <T> T createClient(String url, Class<T> clazz) {
+        return createClient(url, clazz, null);
+    }
+
+    public static <T> T createClient(String url, Class<T> clazz, T fallback) {
         ObjectMapper mapper = new ObjectMapper()
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
@@ -19,6 +29,6 @@ public class RestClientFactory {
                 .decoder(new JacksonDecoder(mapper))
                 .logger(new Slf4jLogger(RestClientFactory.class))
                 .logLevel(Logger.Level.FULL)
-                .target(clazz, url);
+                .target(clazz, url, fallback);
     }
 }
