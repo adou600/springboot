fdf02166974321919e054978ff451c8cb4793f37
Adrien Nicolet <adrien.nicolet@zuehlke.com>
2017-05-10 17:27:28 +0200

    monitoring

--- src/main/java/com/zuehlke/MovieServiceAdapter.java
+++ src/main/java/com/zuehlke/MovieServiceAdapter.java
@@ -4,6 +4,9 @@ import com.zuehlke.data.Movie;
 import com.zuehlke.data.MovieServiceResponse;
 import com.zuehlke.data.MovieSummary;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,7 +15,7 @@ import java.util.Optional;
 import static java.util.stream.Collectors.toList;
 
 @Component
-public class MovieServiceAdapter {
+public class MovieServiceAdapter implements HealthIndicator {
 
     private final MovieServiceApiClient moviesApiClient;
 
@@ -31,6 +34,12 @@ public class MovieServiceAdapter {
     public Optional<Movie> getMovieById(int id) {
         MovieServiceResponse response = moviesApiClient.getMovieById(id);
         return Optional.of(new Movie(response.getId(), response.getTitle(), response.getPoster(), response.getPlot(), response.getYear(), response.getGenre(), null));
+    }
+
+    @Override
+    public Health health() {
+        moviesApiClient.getHealth();
+        return Health.up().build();
     }
 }
 
--- src/main/java/com/zuehlke/MovieServiceApiClient.java
+++ src/main/java/com/zuehlke/MovieServiceApiClient.java
@@ -3,6 +3,7 @@ package com.zuehlke;
 import com.zuehlke.data.MovieServiceResponse;
 import feign.Param;
 import feign.RequestLine;
+import org.springframework.boot.actuate.health.Health;
 
 import java.util.List;
 
@@ -12,4 +13,7 @@ public interface MovieServiceApiClient {
 
     @RequestLine("GET /api/v1/movies/{id}")
     MovieServiceResponse getMovieById(@Param("id") long id);
+
+    @RequestLine("GET /health")
+    void getHealth();
 }
